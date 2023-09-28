e08fca6d00d9f028cd573dcf624607d2c5440124
CaptainIndia <40968890+Sakarmit@users.noreply.github.com>
2023-09-28 18:01:23 -0400

    Added Multi page App (Project 4 Problem 4)

---
+++ p4.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+import States from './components/states/States';
+import Example from './components/example/Example';
+
+ReactDOM.render(
+    <DynamicView />,
+    document.getElementById('dynamic-react'),
+);
+
+export default function DynamicView() {
+    const [showStates, setShowStates] = useState(false);
+
+    const toggleModel = () => {
+        setShowStates((prevShowStates) => !prevShowStates);
+    }
+
+    return (
+        <div>
+            <button onClick={() => {toggleModel()}}>
+                {!showStates && 'Show States'}
+                {showStates && 'Show Example'}
+            </button>
+            {!showStates && <Example />}
+            {showStates && <States />}
+        </div>
+    )
+}
