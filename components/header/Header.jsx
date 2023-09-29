e5c3ca8d1161d9d1bd0715e1667eb1a81ead4d9c
KGerrishUNCC <111810589+KGerrishUNCC@users.noreply.github.com>
2023-09-29 12:55:36 -0400

    new commit

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -19,7 +19,7 @@ export default function DynamicView() {
             <div>
                 <header>
                     <h2>Header</h2>
-
+                    <img src='./happyguy.jpeg' alt="happyguy"></img>
                 </header>
             </div>
         </div>
