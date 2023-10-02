ec28558aa22d432d16dd01f769d571aa7fd2e9ec
KGerrishUNCC <111810589+KGerrishUNCC@users.noreply.github.com>
2023-10-02 13:04:56 -0400

    new commit

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -10,8 +10,8 @@ export default class Header extends React.Component {
         return (
             <div>
                 <header>
-                    <h2>Header</h2>
-                    <img src="/components/header/happyguy.jpeg" alt="happy3guy"></img>
+                    <h2>Header&nbsp;<img src="/components/header/happyguy.jpeg" alt="happy3guy"></img></h2>
+
                 </header>
             </div>
         );
